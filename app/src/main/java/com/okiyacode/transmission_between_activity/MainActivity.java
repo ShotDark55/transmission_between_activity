cfc35a24feced82630a4a1dfd2faf3b5de5495c3
= <=>
2024-12-07 19:35:31 +0100

    first upload create login page

---
+++ app/src/main/java/com/okiyacode/transmission_between_activity/MainActivity.java
@@ -0,0 +1,41 @@
+package com.okiyacode.transmission_between_activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private EditText username;
+    private EditText password;
+
+
+    private void setup() {
+        login = findViewById(R.id.loginID);
+        username = findViewById(R.id.usernameID);
+        password = findViewById(R.id.passwordID);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setup();
+
+    }
+}
