fa64b7e4f25ff067756a4c2da68ff0e4b4d8e145
= <=>
2024-12-07 19:50:43 +0100

    add welcome screen

--- app/src/main/java/com/okiyacode/transmission_between_activity/MainActivity.java
+++ app/src/main/java/com/okiyacode/transmission_between_activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.okiyacode.transmission_between_activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     private EditText username;
     private EditText password;
-
+    private Context context;
 
     private void setup() {
         login = findViewById(R.id.loginID);
         username = findViewById(R.id.usernameID);
         password = findViewById(R.id.passwordID);
-
+        context = this;
     }
 
     @Override
@@ -36,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         setup();
+
+        login.setOnClickListener(v -> {
+            String userText = username.getText().toString();
+            String passText = password.getText().toString();
+
+            if(userText.isEmpty() || passText.isEmpty()) {
+                Toast.makeText(context, "please add user and password ",Toast.LENGTH_SHORT).show();
+            }else{
+                Intent intent = new Intent(context, MainActivity2.class);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }
